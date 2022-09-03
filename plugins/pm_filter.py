144296ae4ee80483141d662a04df4f3266e07f21
devilcomof <112283064+devilcomof@users.noreply.github.com>
2022-09-03 23:05:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -38,18 +38,18 @@ async def give_filter(client, message):
     if settings['auto_ffilter']:
         await auto_filter(client, message)
 
-@Client.on_callback_query(filters.regex(r"^next"))
+@Client.on_callback_query(❤️‍🔥filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+        return await query.answer(❤️‍🔥script.ALRT_TXT.format(❤️‍🔥query.from_user.first_name), show_alert=True)
     try:
         offset = int(offset)
     except:
         offset = 0
     search = BUTTONS.get(key)
     if not search:
-        await query.answer(script.OLD_ALRT_TXT.format(query.from_user.first_name),show_alert=True)
+        await query.answer(❤️‍🔥script.OLD_ALRT_TXT.format(❤️‍🔥query.from_user.first_name),show_alert=True)
         return
 
     files, n_offset, total = await get_search_results(search, offset=offset, filter=True)
@@ -65,7 +65,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'❤️‍🔥files#{file.file_id}'
+                    text=f"[❤️‍🔥{get_size(file.file_size)}] ❤️‍🔥{file.file_name}", callback_data=f'❤️‍🔥files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -74,11 +74,11 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"❤️‍🔥{file.file_name}", callback_data=f'❤️‍🔥files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
-                    callback_data=f'files_#{file.file_id}',
+                    text=f"❤️‍🔥{get_size(file.file_size)}",
+                    callback_data=f'❤️‍🔥files_#{file.file_id}',
                 ),
             ]
             for file in files
