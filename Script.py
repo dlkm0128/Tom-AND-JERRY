486e375c54014f7ac799f8bfd79f0d27abefbb87
devilcomof <112283064+devilcomof@users.noreply.github.com>
2022-09-03 22:01:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,8 +5,7 @@ class script(object):
 Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
     ABOUT_TXT = """╭•━━━━━━━━━━━━━━━➣ 
 ║┣⪼❈ 𝙼𝚈 𝙽𝙰𝙼𝙴: JOHN DURAIRAJ
-║┣⪼❈ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: {}
-║┣⪼❈ DEVELOPER: <a href=https://t.me/AFxSU>⚠️☬𝙴𝚁𝚁𝙾𝚁࿐ ᯤ</a>
+║┣⪼❈ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁:  <a href=https://t.me/AFxSU>⚠️☬𝙴𝚁𝚁𝙾𝚁࿐ ᯤ</a>
 ║┣⪼❈ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ║┣⪼❈ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ║┣⪼❈ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
