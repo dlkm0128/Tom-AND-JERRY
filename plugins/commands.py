a5f483bab21eb4f1519c2cb81d03b94b378fdd7b
AT3MOVIESBOT <112283064+AT3MOVIESBOT@users.noreply.github.com>
2022-09-05 22:57:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,12 +59,12 @@ async def start(client, message):
         await message.reply_sticker("CAACAgIAAxkBAAEFwl1jFch8E4Oj4gPdVMkcQeIYOwABoIAAAlQWAALQe0FJ_LlY2fNGVigpBA") 
         await asyncio.sleep(1)
         await m.delete() 
-               [
                  [
+              [
                     InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
   
-                ]
-                  ]
+                    ]
+           ]
 
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -118,11 +118,11 @@ async def start(client, message):
         await asyncio.sleep(1) 
         await m.delete() 
                  [
-                   [
+              [
                     InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
   
                 ]
-                   ]
+            ]
 
         await message.reply_photo(
             photo=random.choice(PICS),
