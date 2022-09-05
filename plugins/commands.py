3d6ea5664877aa29f0538b8a7460a8dde79e4574
AT3MOVIESBOT <112283064+AT3MOVIESBOT@users.noreply.github.com>
2022-09-05 23:23:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,7 +56,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Oᴜʀ Mᴀɪɴ Cʜᴀɴɴᴇʟ ✇', url='https://t.me/at3_movies')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)  
-        m=await message.reply_sticker("CAACAgIAAxkBAAEFwl1jFch8E4Oj4gPdVMkcQeIYOwABoIAAAlQWAALQe0FJ_LlY2fNGVigpBA") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAEFw79jFjdEdh4Lavrmt0K9zN6JttblxQACmgADyJRkFCxl4eFc7yVqKQQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -107,7 +107,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Oᴜʀ Mᴀɪɴ Cʜᴀɴɴᴇʟ ✇', url='https://t.me/at3_movies')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)     
-        m=await message.reply_sticker("CAACAgUAAxkBAAEFwltjFchvjUS4Y7AJ3PbPb6hBOPAPTwACBAADwSQxMYnlHW4Ls8gQKQQ") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAEFw79jFjdEdh4Lavrmt0K9zN6JttblxQACmgADyJRkFCxl4eFc7yVqKQQ") 
         await asyncio.sleep(1) 
         await m.delete() 
         await message.reply_photo(
