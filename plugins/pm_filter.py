263d972c196dd7206c7c1983986620ecb6b14358
AT3MOVIESBOT <112283064+AT3MOVIESBOT@users.noreply.github.com>
2022-09-05 23:44:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -497,7 +497,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.edit_text(
             text="▣ ▣ ▣"
         )
-            text=script.HELP_TXT.format(query.from_user.mention),
+            text=script.HELP_TXT.
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
