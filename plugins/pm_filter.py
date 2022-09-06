d08e9587551473ae026fea687ce58dcaa74c153b
AT3MOVIESBOT <112283064+AT3MOVIESBOT@users.noreply.github.com>
2022-09-06 09:35:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,7 +65,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'❤️‍🔥files#{file.file_id}'
+                    text=f"❤️‍🔥[{get_size(file.file_size)}]❤️‍🔥{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -74,10 +74,10 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"❤️‍🔥{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"❤️‍🔥{get_size(file.file_size)}❤️‍🔥",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -723,7 +723,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"❤️‍🔥[{get_size(file.file_size)}]❤️‍🔥{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -732,11 +732,11 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}",
+                    text=f"❤️‍🔥{file.file_name}",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"❤️‍🔥{get_size(file.file_size)}❤️‍🔥",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
